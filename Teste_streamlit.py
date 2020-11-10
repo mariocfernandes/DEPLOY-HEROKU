7f8a7a48aba3565a9651251cc2e73678b1ecbce1
MARIO CESAR FERNANDES <mariocf0709@gmail.com>
2020-11-10 15:10:43 -0300

    Update Teste_streamlit.py

--- Teste_streamlit.py
+++ Teste_streamlit.py
@@ -5,9 +5,9 @@ st.title("Dispinibilizando Dados via App")
 
 st.write("""
 # Criando Apps
-Estou abrindo a possibiliade de a partir de agora
-criar aplicações ( consultas e relatorios das nossas base de dados ) e
-disponiblizar via App. Disponibilizo o link e Nós e as gerencias poderão ter acessos
+Estou abrindo a possibilidade de a partir de agora de começarmos a 
+criar aplicações ( consultas e relatorios das nossas base de dados , etc ) e
+disponiblizar via App. Disponibilizo o link e Nós e as Gerencias & Vendedores  poderão ter acessos
 a consultas e relatorios que começaremos a disponibilizar. Acho uma importante iniciativa de 
 começar a disponibilizar informações via App, onde podemos acessar via navegador no nosso celular.
 """)
